771cfb20204d4360127bec874f8314aaa5474ef8
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-11 20:25:24 +0800

    test

---
+++ Test1.java
@@ -0,0 +1,11 @@
+//老师写的最简单的程序，完成 1+1， 体会
+
+public class Test1 {
+
+               public static void main(String[] args)  {
+      
+                              int res = 1 + 1;
+                              //显示
+                              System.out.println("结果=" + res);   
+               }
+}
