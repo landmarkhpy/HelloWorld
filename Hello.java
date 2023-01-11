0b98f0a894537736158d49f2f5385726ed7297ef
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-11 21:34:07 +0800

    test

---
+++ Hello.java
@@ -0,0 +1,16 @@
+
+//这是java的快速入门，显示java的开发步骤
+//对代号的相关说明
+//1. public class Hello 表示你好是一个类，是一个public公开的类
+//2. Hello{ } 表示一个类型的开始和结束
+//3. public static void main(String[] args) 表示一个主方法，即我们程序的入口
+//4. main() {} 表示方法的开始和结束
+//5. System.out.println("hello,world~"); 显示输出"hello,world~"到屏幕
+//6. ;表示语句结束
+public class Hello {
+
+    //编写一个主要方法
+	public static void main(String[] args) {
+		System.out.println("hello,world~");
+	}
+}
