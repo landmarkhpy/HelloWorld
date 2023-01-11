ae2093d022f103ed07ca61eb7c5ea78fa9a5f299
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-11 20:57:05 +0800

    test

---
+++ HideAndSeek.java
@@ -0,0 +1,9 @@
+public class HideAndSeek {
+	public static void main(String[] args) {
+		System.out.println("Let's play hide + andseek.");
+		System.out.println("Three...");
+		System.out.println("Two...");
+		System.out.println("One...");
+        System.out.println("Ready or not. here I come!");     
+	}
+}
