e69c22b9145ea7f9c45193113387c2c5b40e7080
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-14 18:58:22 +0800

    test

---
+++ LanguageFacts.java
@@ -0,0 +1,9 @@
+public class LanguageFacts {
+	public static void main(String[] args) {
+	  // Press enter  or return on your keyboard after each semicolin!
+	  System.out.println("programming is very good!");
+	  System.out.println("Java is a clss-based language.");
+	  System.out.println("Java classes have a 'main' method.");
+	  System.out.println("Java statemnts end with a semicolon.");	
+	}
+}
