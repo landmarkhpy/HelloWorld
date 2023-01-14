ea8a25dc47cf4e0d0611784a06d2bee90b9986ee
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-14 11:31:56 +0800

    test

---
+++ comment.java
@@ -0,0 +1,17 @@
+public class comment {
+	public static void main(String[] agrs) {
+		//完成代码
+		System.out.println("相加");
+
+	    //示意 => 可读性很好
+	    //下面代码完成 两个数相加
+	    //定义变量
+	    int n1 = 10;
+	    int n2 = 30;
+	    //求和
+	    int sum = n1 + n2;
+	    //输出结果
+	    System.out.println("结果=" + sum);
+	}
+
+}
