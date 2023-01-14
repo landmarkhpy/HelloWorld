1de0e5beae2d9efa11d4d63ba443cea50ecc41a8
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-14 10:03:54 +0800

    test

---
+++ ChangeCharExer01.java
@@ -0,0 +1,9 @@
+
+public class ChangeCharExer01 {
+
+    //编写一个main方法    
+	public static void main(String[] args) {
+        //完成代码
+        System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+	}
+}
