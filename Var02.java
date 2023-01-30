2283bce873e37e8f8b2d422201d0cee9e3828485
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-30 12:19:00 +0800

    test

---
+++ Var02.java
@@ -0,0 +1,17 @@
+
+public class Var02 {
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		//记录人的信息
+		int age = 30;
+		double score = 88.6;
+		char gender = '男';
+		String name = "king";
+		//输出信息，快捷键
+		System.out.println(age);
+		System.out.println(gender);
+		System.out.println(score);
+		System.out.println(name);
+	}
+}
