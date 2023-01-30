48cd0f8aa26db84a0515cdaecd7a1b0938ab91f0
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-30 16:04:09 +0800

    test

---
+++ IntDetail.java
@@ -0,0 +1,16 @@
+
+
+public class IntDetail {
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		
+		//Java的整型常量（具体值）默认为int型，声明long型常量须后加‘l’或‘L’
+		int n1 = 1;//4个字节
+		//int n2 = 1L;//L为long[8]，int[4],容量不够
+		long n3 = 1L;//
+	System.out.println(n1);
+	System.out.println(n3);
+	}
+}
+	
