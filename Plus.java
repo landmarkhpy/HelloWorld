43e1aedbf7f5193bf0ca108e747a2c96e101e3d8
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-30 13:35:55 +0800

    test

---
+++ Plus.java
@@ -0,0 +1,12 @@
+
+
+public class Plus {
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		System.out.println(100+98);
+		System.out.println("100"+98);
+		System.out.println(100+3+"hello");
+		System.out.println("hello"+100+3);
+	}
+}
