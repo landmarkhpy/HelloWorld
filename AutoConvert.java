fdb8e45fc9aa36afe9ccbb09390e76209bc1b755
HanPengYang1776 <hanpengyang001@gmail.com>
2023-01-31 20:31:51 +0800

    test

---
+++ AutoConvert.java
@@ -0,0 +1,12 @@
+
+public class AutoConvert {
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		//演示自动转换
+		int num = 'a';//ok char -> int 
+		double d1 = 80; //ok int -> double
+		System.out.println(num);//
+		System.out.println(d1);//因为是个整数，整数先转换成doble然后在赋给d1 
+	}
+}
