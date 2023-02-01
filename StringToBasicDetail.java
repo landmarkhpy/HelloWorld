dc462c40e56baaa6dfba1460b4d13ed66123cae2
HanPengYang1776 <hanpengyang001@gmail.com>
2023-02-01 22:31:42 +0800

    test

---
+++ StringToBasicDetail.java
@@ -0,0 +1,12 @@
+
+public class StringToBasicDetail {
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		
+		String str = "123";
+		//转成int
+		int n1 = Integer.parseInt(str);
+		System.out.println(n1);
+	}
+}
