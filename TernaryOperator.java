60819fb87b133d6142e436700fcc4b7194d4174f
HanPengYang1776 <hanpengyang001@gmail.com>
2023-02-07 21:55:49 +0800

    test

---
+++ TernaryOperator.java
@@ -0,0 +1,22 @@
+//三元运算符使用
+
+public class TernaryOperator {
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 99;
+		//解读
+		//1. a > b 为 false
+		//2. 返回 b--，先返回b的值，然后在b-1
+		//3. 返回的结果是99
+//		int result = a > b ? a++ :b--;
+//		System.out.println("result=" + result);
+		
+		int result = a < b ? a++ :b--;
+		System.out.println("result=" + result);
+		System.out.println("a=" + a);
+		System.out.println("b=" + b);
+	}
+}
---
+++ TernaryOperatorDetail.java
@@ -0,0 +1,16 @@
+//三元运算符细节
+
+public class TernaryOperatorDetail {
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		//表达式1和表达式2要为可以赋给接收变量的类型（或可以自动转换）
+		int a = 3;
+		int b = 8;
+		int c = a > b ? a : b;
+//		int c = a > b ? (int)1.1 : (int)3.4;//可以
+//		double d = a > b ? a : b + 3;//可以的，满足 int -> double
+		System.out.println("c=" + c);
+	    
+	}
+}
